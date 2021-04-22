9a5e1d9baf9fb45891fd6942f3962bc342c34f51
yhnjeong <yhnjeong@gmail.com>
2021-04-22 13:41:52 +0900

    created basic javascript files

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Im Working. Im JS. Im Beautiful. Im worth it.');
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+    title.addEventListener("click", handleClick);
+}
+init();
