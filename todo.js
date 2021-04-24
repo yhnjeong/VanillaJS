deff83d90bd18c73fb96d4a1c8c2ed78dd7101c6
yhnjeong <yhnjeong@gmail.com>
2021-04-25 08:53:53 +0900

    created bg.js. etc.

--- todo.js
+++ todo.js
@@ -1,21 +1,36 @@
-const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = toDoForm.querySelector("input"),
-    toDoList = document.querySelector(".js-toDoList");
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos() {
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+let idNumbers = 1;
+
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = toDos.length + 1;
+    const newId = idNumbers;
+    idNumbers += 1;
     delBtn.innerText="❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText=text;
     li.appendChild(delBtn);
     li.appendChild(span);
